098a6d4790b2f129f84b987faeb101737090f030
Travis Gerke <tgerke@mail.harvard.edu>
2022-09-30 12:43:25 -0700

    add log in/out buttons and user info

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <button @click="login">Log in</button>
+    <button @click="logout">Log out</button>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <pre v-if="isAuthenticated">
+    <code>{{ user }}</code>
+  </pre>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  import { useAuth0 } from '@auth0/auth0-vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  export default {
+    setup() {
+      const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+      return {
+        login: () => {
+          loginWithRedirect();
+        },
+        logout: () => {
+          logout({ returnTo: window.location.origin });
+        },
+        user,
+        isAuthenticated
+      };
+    }
+  };
+</script>

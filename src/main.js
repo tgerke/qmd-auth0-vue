94c69a8dbe8de9c178ffa0583e28444988a968e3
Travis Gerke <tgerke@mail.harvard.edu>
2022-09-30 12:21:19 -0700

    register the plugin

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createAuth0 } from '@auth0/auth0-vue';
+
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: "dev-vs2e5wst.us.auth0.com",
+    client_id: "8jP639CYGEdGBIhoKRqE867nAkt1Ibmu",
+    redirect_uri: window.location.origin
+  })
+);
+
+app.mount('#app')
